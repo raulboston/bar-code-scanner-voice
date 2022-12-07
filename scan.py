6e77c2a60e19b58a39419fb742cbe21966c9c46e
raulboston <raulboston10@gmail.com>
2022-12-07 14:08:32 -0700

    principal

--- scan.py
+++ scan.py
@@ -1,5 +1,8 @@
 from gtts import gTTS
+import speech_recognition as sr
 import os
+from colorama import Back, Fore, init
+init(autoreset=True)
 
 '''Aquí van todas las posibles alternativas para el lector.'''
 items = {"4005900618993":{"name":"Nivea Men Fresh",
@@ -29,23 +32,34 @@ def Procedimiento():
     continuar = True
 
     while continuar == True:
-        print("Escanea tu codigo de barras")
+        print(Fore.GREEN + "Escanea tu codigo de barras")
         value = input();
         modo_manual="S_CMD_MT00"
         modo_auto="S_CMD_020F"
-        end_code="0"
+        end_code="A0000A"
         if value == end_code:
             break
         if value == modo_manual:
             print("Ahora estás en modo manual")
+            textManual = ("Modo manual")
+            language = "es-us"
+            speechManual = gTTS(text= textManual, lang=language, slow=False)
+            speechManual.save("textomanual.mp3")
+            os.system("start textomanual.mp3")
+            
         if value == modo_auto:
             print("Ahora estás en modo automatico")
+            textAuto = ("Modo automatico")
+            language = "es-us"
+            speechAuto = gTTS(text= textAuto, lang=language, slow=False)
+            speechAuto.save("textoauto.mp3")
+            os.system("start textoauto.mp3")
 
         for key in items.keys():
             if key==value:
                 item = items[key]
                 print(f"Esto es: {item['name']}, El precio es: {item['precio']} pesos mexicanos")
-                text= (f"Esto es: {item['name']}, El precio es: {item['precio']} pesos mexicanos")
+                text= (f"{item['name']}, El precio es: {item['precio']} pesos mexicanos")
                 total_bill += item['precio']
                 bill.append(item)
                 contador + 1
@@ -56,27 +70,22 @@ def Procedimiento():
                 os.system("start texto1.mp3")
                 break
         else:
-            print("Objeto no encontrado",value)
-            '''add = int(input("Cantidad de articulos a ingresar: "))
-            if(add!=0):
-                from database import articulo
-            else:
-                Procedimiento()'''
-
+            print(Fore.RED + "Objeto no encontrado",value)
+            continue 
+            
     for i,item in enumerate(bill):
          print(f"{i+1}. {item['name'],item['precio']}")
 
-    '''en caso de existir menos de un articulo, no hará falta decir el precio final'''
-    if contador>0:
-        print(f'\nTu gasto total es de: ${total_bill} pesos mexicanos')
-        textTotal= (f'\nTu gasto total es de: {total_bill} pesos mexicanos')
-        language = "es-us"
-        speech2 = gTTS(text= textTotal, lang=language, slow=False)
-        speech2.save("textTotal.mp3")
-        os.system("start textTotal.mp3")
+
+    print(Back.WHITE + Fore.BLACK + f'\nTu gasto total es de: ${total_bill} pesos mexicanos')
+    textTotal= (f'\nTu gasto total es de: {total_bill} pesos mexicanos')
+    language = "es-us"
+    speech2 = gTTS(text= textTotal, lang=language, slow=False)
+    speech2.save("textTotal.mp3")
+    os.system("start textTotal.mp3")
 
     '''lanzamiento de menu de inicio luego de finalizar un registro de articulos'''
-    print("Ingresa cualquier texto para continuar \no escanea el codigo para apagar el escaner")
+    print("Presiona enter para continuar \no escanea el codigo para apagar el escaner")
     finish = "^&041&^"
     goback = input();
     if goback == finish:
